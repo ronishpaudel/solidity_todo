117c1be96d8d3024f7b11852661e83833a00673e
robuGdada <paudelronish@gmail.com>
2025-03-15 11:25:29 +0545

    Update Hardhat configuration and dependencies for improved compatibility

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,10 @@
-// Replace this:
-// import "@nomicfoundation/hardhat-toolbox";
-
-// With this:
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
 
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.28", // Use your Solidity version
   networks: {
     localhost: {
-      url: "http://127.0.0.1:8545",
-    },
-  },
+      url: "http://127.0.0.1:8545"
+    }
+  }
 };
